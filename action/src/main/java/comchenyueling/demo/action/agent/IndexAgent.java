41fbe4856d52846a025835e043144e21cad7131b
chenyueling <chen_yueling@163.com>
2016-03-31 16:34:56 +0800

    new file:   .gitignore 	new file:   action/pom.xml 	new file:   action/src/main/java/comchenyueling/demo/action/agent/IndexAgent.java 	new file:   action/src/main/java/comchenyueling/demo/action/agent/form/IndexForm.java 	new file:   action/src/main/java/comchenyueling/demo/action/agent/vo/IndexVo.java 	new file:   action/src/main/webapp/WEB-INF/web.xml 	new file:   interfaces/pom.xml 	new file:   pom.xml 	new file:   service/pom.xml 	new file:   service/src/main/webapp/WEB-INF/web.xml :

---
+++ action/src/main/java/comchenyueling/demo/action/agent/IndexAgent.java
@@ -0,0 +1,28 @@
+package comchenyueling.demo.action.agent;
+
+/**
+ * 　　　　　　　 ┏┓   ┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　  ┃　　　┃
+ * 　　　　　　　  ┃　　　┃ + + + +
+ * 　　　　　　　  ┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　  ┃　　　┃ + 　　　　神兽保佑,代码无BUG
+ * 　　　　　　　  ┃　　　┃
+ * 　　　　　　　  ┃　　　┃　　+
+ * 　　　　　　　  ┃　 　　┗━━━┓ + +
+ * 　　　　　　　  ┃ 　　　　　　　┣┓
+ * 　　　　　　　  ┃ 　　　　　　　┏┛
+ * 　　　　　　　  ┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　┃┫┫ ┃┫┫
+ * 　　　　　　　　　┗┻┛ ┗┻┛+ + + +
+ * Created by Paul on 2016/3/30.
+ */
+public class IndexAgent {
+}
---
+++ action/src/main/java/comchenyueling/demo/action/agent/form/IndexForm.java
@@ -0,0 +1,29 @@
+package comchenyueling.demo.action.agent.form;
+
+/**
+ * 　　　　　　　 ┏┓   ┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　  ┃　　　┃
+ * 　　　　　　　  ┃　　　┃ + + + +
+ * 　　　　　　　  ┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　  ┃　　　┃ + 　　　　神兽保佑,代码无BUG
+ * 　　　　　　　  ┃　　　┃
+ * 　　　　　　　  ┃　　　┃　　+
+ * 　　　　　　　  ┃　 　　┗━━━┓ + +
+ * 　　　　　　　  ┃ 　　　　　　　┣┓
+ * 　　　　　　　  ┃ 　　　　　　　┏┛
+ * 　　　　　　　  ┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　┃┫┫ ┃┫┫
+ * 　　　　　　　　　┗┻┛ ┗┻┛+ + + +
+ * Created by Paul on 2016/3/30.
+ * 表单接收类
+ */
+public class IndexForm {
+}
