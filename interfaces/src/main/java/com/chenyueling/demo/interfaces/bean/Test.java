52951cb880986b566260c7a98330a04fde64dd41
chenyueling <chen_yueling@163.com>
2016-03-31 16:45:26 +0800

    new file:   interfaces/src/main/java/com/chenyueling/demo/interfaces/bean/Test.java

---
+++ interfaces/src/main/java/com/chenyueling/demo/interfaces/bean/Test.java
@@ -0,0 +1,28 @@
+package com.chenyueling.demo.interfaces.bean;
+
+/**
+ * 　　　　　　　 ┏┓   ┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　  ┃　　　┃
+ * 　　　　　　　  ┃　　　┃ + + + +
+ * 　　　　　　　  ┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　  ┃　　　┃ + 　　　　神兽保佑,代码无BUG
+ * 　　　　　　　  ┃　　　┃
+ * 　　　　　　　  ┃　　　┃　　+
+ * 　　　　　　　  ┃　 　　┗━━━┓ + +
+ * 　　　　　　　  ┃ 　　　　　　　┣┓
+ * 　　　　　　　  ┃ 　　　　　　　┏┛
+ * 　　　　　　　  ┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　┃┫┫ ┃┫┫
+ * 　　　　　　　　　┗┻┛ ┗┻┛+ + + +
+ * Created by Paul on 2016/3/30.
+ */
+public class Test {
+}
